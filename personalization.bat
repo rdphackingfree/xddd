f8a356d7ba1543f2a366da3b7da4761482b6067d
Metehan Selvi <81955421+Metehanse@users.noreply.github.com>
2021-05-18 12:57:26 +0300

    Update personalization.bat

--- personalization.bat
+++ personalization.bat
@@ -1,8 +1,6 @@
 @echo off
 >nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
-if '%errorlevel%' NEQ '0' (
-goto a
-) else ( goto b )
+if '%errorlevel%' NEQ '0' ( goto a ) else ( goto b )
 :a
 echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
 echo UAC.ShellExecute "%~s0", "", "", "runas", 1 >> "%temp%\getadmin.vbs"
