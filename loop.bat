2f0dd949f72e6f2968d9f4043c5b793460b90f6e
Metehan Selvi <81955421+Metehanse@users.noreply.github.com>
2021-05-18 10:07:14 +0300

    Update loop.bat

--- loop.bat
+++ loop.bat
@@ -1,6 +1,6 @@
 @echo off
 echo RDP started successfully.
-tasklist | find /i "ngrok.exe" >Nul && goto check || echo "Unable to get NGROK tunnel, make sure NGROK_AUTH_TOKEN is correct in Settings > Secrets > Repository secret. Maybe your previous RDP is still running: https://dashboard.ngrok.com/status/tunnels" & ping 127.0.0.1 >Nul & exit
+tasklist | find /i "ngrok.exe" >Nul && goto check || echo "Unable to get NGROK tunnel, make sure NGROK_AUTH_TOKEN is correct in Settings > Secrets > Repository secret. Maybe your previous RDP is still running: https://dashboard.ngrok.com/status/tunnels " & ping 127.0.0.1 >Nul & exit
 :check
 ping 127.0.0.1 > nul
 cls
