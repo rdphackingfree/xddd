8aa97bffcf7cd5dfff388819ef86a8e43305017f
Metehan Selvi <81955421+Metehanse@users.noreply.github.com>
2021-05-18 10:49:52 +0300

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,5 +1,5 @@
 @echo off
-SET /A password="@NgrokRDP"
+SET /A password="@ngrokRDP"
 del /f "C:\Users\Public\Desktop\Epic Games Launcher.lnk" > out.txt 2>&1
 net config server /srvcomment:"Windows Server 2019 By AdityaGans2542" > out.txt 2>&1
 REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer" /V EnableAutoTray /T REG_DWORD /D 0 /F > out.txt 2>&1
@@ -13,10 +13,9 @@ sc config Audiosrv start= auto >nul
 sc start audiosrv >nul
 ICACLS C:\Windows\Temp /grant administrator:F >nul
 ICACLS C:\Windows\installer /grant administrator:F >nul
-echo Berhasil Menginstall!, Jika RDPnya Mati Silahkan Rebuild Lagi!
-echo IP:
-tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Unable to get NGROK tunnel, make sure NGROK_AUTH_TOKEN is correct in Settings > Secrets > Repository secret. Maybe your previous RDP is still running: https://dashboard.ngrok.com/status/tunnels "
+echo If the RDP dead, please rerun workflow.
+echo IP: tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Unable to get NGROK tunnel, make sure NGROK_AUTH_TOKEN is correct in Settings > Secrets > Repository secret. Maybe your previous RDP is still running: https://dashboard.ngrok.com/status/tunnels "
 echo Username: administrator
-echo Password: administrator
+echo Password: %password%
 echo Login to your RDP!!
 ping -n 10 127.0.0.1 >nul
