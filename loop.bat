370b6a1767717ad61c83f978586f6250a70909bd
Metehan Selvi <81955421+Metehanse@users.noreply.github.com>
2021-05-18 15:57:36 +0300

    Add files via upload

--- loop.bat
+++ loop.bat
@@ -1,11 +1,8 @@
-:: Coded by ?, remade my Metehanse
 @echo off
-echo Coded by ?, remade my Metehanse
-echo RDP started successfully.
-tasklist | find /i "ngrok.exe" >Nul && goto check || echo "Unable to get NGROK tunnel, make sure NGROK_AUTH_TOKEN is correct in Settings > Secrets > Repository secret. Maybe your previous RDP is still running: https://dashboard.ngrok.com/status/tunnels " & ping 127.0.0.1 >Nul & exit
+echo FreeRDP By NaufalCream!
+tasklist | find /i "ngrok.exe" >Nul && goto check || echo "Tidak bisa mendapatkan NGROK tunnel, pastikan NGROK_AUTH_TOKEN benar di Settings> Secrets> Repository secret. Mungkin VM Anda sebelumnya masih berjalan: https://dashboard.ngrok.com/status/tunnels" & ping 127.0.0.1 >Nul & exit
 :check
 ping 127.0.0.1 > nul
 cls
-set /a num=(%RANDOM%*9000/32768)+1000
-echo %num%
+echo FreeRDP By NaufalCream!
 goto check
