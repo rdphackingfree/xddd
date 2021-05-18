ba289c3a086ccbfae4f38be564c9e77c1aeafb2a
Metehan Selvi <81955421+Metehanse@users.noreply.github.com>
2021-05-18 16:07:26 +0300

    Add files via upload

--- settings.bat
+++ settings.bat
@@ -1,26 +1,39 @@
+:: Coded by ?, remade my Metehanse
 @echo off
 >nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
-if '%errorlevel%' NEQ '0' ( goto UACPrompt ) else ( goto KetemuAdmin )
-:UACPrompt
+if '%errorlevel%' NEQ '0' ( goto a ) else ( goto b )
+:a
 echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
 echo UAC.ShellExecute "%~s0", "", "", "runas", 1 >> "%temp%\getadmin.vbs"
 "%temp%\getadmin.vbs"
 exit /B
-:KetemuAdmin
+:b
 if exist "%temp%\getadmin.vbs" ( del "%temp%\getadmin.vbs" )
 pushd "%CD%"
 CD /D "%~dp0"
-REG ADD HKLM\SYSTEM\CurrentControlSet\Control\ComputerName\ComputerName /v ComputerName /t REG_SZ /d NaufalRDP /f
-REG ADD HKLM\SYSTEM\CurrentControlSet\Control\ComputerName\ActiveComputerName\ /v ComputerName /t REG_SZ /d NaufalRDP /f
-REG ADD HKLM\SYSTEM\CurrentControlSet\Services\Tcpip\Parameters\ /v Hostname /t REG_SZ /d NaufalRDP /f
-REG ADD HKLM\SYSTEM\CurrentControlSet\Services\Tcpip\Parameters\ /v "NV Hostname" /t REG_SZ /d NaufalRDP /f
-REG ADD HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\OEMInformation /v Manufacturer /t REG_SZ /d "LumminaHost, Inc." /f
-REG ADD HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\OEMInformation /v Model /t REG_SZ /d "LumminaHost Virtual Machine" /f
-REG ADD HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\OEMInformation /v SupportURL /t REG_SZ /d "https://github.com/EmanSaputra/FreeRDP/issues" /f
-REG ADD "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v TaskbarSmallIcons /t REG_DWORD /d 1 /f
+REG ADD "HKCU\Control Panel\Desktop" /v WallPaper /f /t REG_SZ /d C:\Windows\wallpaper.png
+REG ADD "HKCU\Control Panel\Desktop" /v UserPreferencesMask /f /t REG_BINARY /d 9E12038010000000
+REG ADD HKLM\SYSTEM\CurrentControlSet\Control\ComputerName\ComputerName /v ComputerName /t REG_SZ /d WindowsRDP /f
+REG ADD HKLM\SYSTEM\CurrentControlSet\Control\ComputerName\ActiveComputerName\ /v ComputerName /t REG_SZ /d WindowsRDP /f
+REG ADD HKLM\SYSTEM\CurrentControlSet\Services\Tcpip\Parameters\ /v Hostname /t REG_SZ /d WindowsRDP /f
+REG ADD HKLM\SYSTEM\CurrentControlSet\Services\Tcpip\Parameters\ /v "AD Host" /t REG_SZ /d WindowsRDP /f
+REG ADD HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\OEMInformation /v Manufacturer /t REG_SZ /d "AdHost" /f
+REG ADD HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\OEMInformation /v Model /t REG_SZ /d "AdHost Virtual Machine" /f
+REG ADD HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\OEMInformation /v SupportURL /t REG_SZ /d "https://github.com/Metehanse/WindowsRDP/issues" /f
+REG ADD HKLU\Software\Microsoft\Windows\CurrentVersion\Explorer\VisualEffects /v VisualFXSetting /t REG_DWORD /d 2 /f
+REG ADD "HKLU\Control Panel\Desktop\WindowMetrics" /v MinAnimate /t REG_SZ /d 0 /f
+REG ADD "HKLU\Control Panel\Desktop" /v DragFullWindows /t REG_SZ /d 1 /f
+REG ADD "HKLU\Control Panel\Desktop" /v FontSmoothing /t REG_SZ /d 2 /f
+REG ADD "HKLU\Control Panel\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v ListviewShadow /t REG_SZ /d 0 /f
+REG ADD "HKLU\Control Panel\Desktop\WindowMetrics" /v MinAnimate /t REG_SZ /d 1 /f
+REG ADD HKLU\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes /v AppsUseLightTheme /t REG_DWORD /d 0 /f
 powershell -Command "& {Invoke-WebRequest https://drive.google.com/uc?id=1E7vYf_b-uaJnlLQwv3ZyPe21u5z-CwVZ -OutFile wallpaper.zip; Expand-Archive wallpaper.zip; Remove-Item wallpaper.zip}"
 @D:\a\wallpaper\wallpaper.exe D:\a\wallpaper\wallpaper.bgi /timer:0
 @RD /S /Q wallpaper
-RUNDLL32.EXE user32.dll,UpdatePerUserSystemParameters
+cls
+echo Personalization complete
+timeout /t 1
+rundll32.exe user32.dll,UpdatePerUserSystemParameters 1, True
+ie4uinit.exe -ClearIconCache
 taskkill /f /im explorer.exe
 start explorer.exe
--- start.bat
+++ start.bat
@@ -1,21 +1,26 @@
+:: Coded by ?, remade my Metehanse
 @echo off
+set username=administrator
+set password=@ngrokRDP
 del /f "C:\Users\Public\Desktop\Epic Games Launcher.lnk" > out.txt 2>&1
-net config server /srvcomment:"Windows Server 2019 By NaufalCream" > out.txt 2>&1
-REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer" /V EnableAutoTray /T REG_DWORD /D 0 /F > out.txt 2>&1
-REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" /f /v Wallpaper /t REG_SZ /d D:\a\settings.bat
-net user administrator @NaufalCream12 /add >nul
-net localgroup administrators administrator /add >nul
-net user administrator /active:yes >nul
+net config server /srvcomment:"Windows Server 2019 By AdityaGans2542" > out.txt 2>&1
+REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer" /v EnableAutoTray /t REG_DWORD /d 0 /f > out.txt 2>&1
+REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" /f /v Settings /t REG_SZ /d D:\a\settings.bat
+net user %username% %password% /add >nul
+net localgroup administrators %username% /add >nul
+net user %username% /active:yes >nul
 net user installer /delete
 diskperf -Y >nul
-sc config Audiosrv start= auto >nul
+sc config Audiosrv start=auto >nul
 sc start audiosrv >nul
-ICACLS C:\Windows\Temp /grant administrator:F >nul
-ICACLS C:\Windows\installer /grant administrator:F >nul
-echo Berhasil Menginstall!, Jika RDPnya Mati Silahkan Rebuild Lagi!
-echo IP:
-tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Tidak bisa mendapatkan NGROK tunnel, pastikan NGROK_AUTH_TOKEN benar di Settings> Secrets> Repository secret. Mungkin VM Anda sebelumnya masih berjalan: https://dashboard.ngrok.com/status/tunnels "
-echo Username: administrator
-echo Password: @NaufalCream12
-echo Silahkan Login Ke RDP Anda!!
+ICACLS C:\Windows\Temp /grant %username%:F >nul
+ICACLS C:\Windows\installer /grant %username%:F >nul
+
+echo If the RDP dead, please rerun workflow.
+ 
+echo|set /p="IP: "
+tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Unable to get NGROK tunnel, make sure NGROK_AUTH_TOKEN is correct in Settings > Secrets > Repository secret. Maybe your previous RDP is still running: https://dashboard.ngrok.com/status/tunnels "
+echo Username: %username%
+echo Password: %password%
+echo Login to your RDP!
 ping -n 10 127.0.0.1 >nul
