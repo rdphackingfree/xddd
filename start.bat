7f046a004de765c8b1110a8f5ef94d71793ac53e
Metehan Selvi <81955421+Metehanse@users.noreply.github.com>
2021-05-18 11:10:29 +0300

    Update start.bat

--- start.bat
+++ start.bat
@@ -13,7 +13,6 @@ sc config Audiosrv start= auto >nul
 sc start audiosrv >nul
 ICACLS C:\Windows\Temp /grant administrator:F >nul
 ICACLS C:\Windows\installer /grant administrator:F >nul
- 
 echo If the RDP dead, please rerun workflow.
  
  
