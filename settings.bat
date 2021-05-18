46f9017172633838903bbe0ee4fc2845d446adf2
Metehan Selvi <81955421+Metehanse@users.noreply.github.com>
2021-05-18 15:59:21 +0300

    Update settings.bat

--- settings.bat
+++ settings.bat
@@ -1,9 +1,6 @@
 @echo off
 >nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
-if '%errorlevel%' NEQ '0' (
-echo Mencari Admin Akses!
-goto UACPrompt
-) else ( goto KetemuAdmin )
+if '%errorlevel%' NEQ '0' ( goto UACPrompt ) else ( goto KetemuAdmin )
 :UACPrompt
 echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
 echo UAC.ShellExecute "%~s0", "", "", "runas", 1 >> "%temp%\getadmin.vbs"
